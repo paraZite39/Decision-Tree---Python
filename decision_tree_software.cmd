ca55e6992d8e5b72a698ccab72207b2d7544417a
paraZite39 <39121521+paraZite39@users.noreply.github.com>
2022-01-17 05:22:47 +0200

    Add files via upload

---
+++ decision_tree_software.cmd
@@ -0,0 +1,6 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'decision_tree_software.exe' with Python DLL being found.
+set PATH=c:\users\user\appdata\local\programs\python\python39;%PATH%
+set PYTHONHOME=c:\users\user\appdata\local\programs\python\python39
+"%~dp0.\decision_tree_software.exe" %*
